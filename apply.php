bfa5ff6fc15193df3787f757412064c9aba8fabc
devkartic <kartic.gharami@gmail>
2024-10-25 11:51:11 +0600

    Working with pdf local

--- apply.php
+++ apply.php
@@ -1,29 +1,25 @@
 <?php
 
-include './dompdf/autoload.inc.php';
-// reference the Dompdf namespace
-use Dompdf\Dompdf;
+if(!empty($_FILES['advanced_psychological_assessment'])) {
 
-if(!empty($_POST)){
+    // Directory where the file will be saved
+    $uploadDir = 'uploads/';
 
-    $html = $_POST['iframe_content'];
-    echo $html;
-    die();
-    // instantiate and use the dompdf class
-    $dompdf = new Dompdf();
-    $dompdf->loadHtml($html);
+    // Access the uploaded file info
+    $fileTmpPath = $_FILES['advanced_psychological_assessment']['tmp_name'];
+    $fileName = $_FILES['advanced_psychological_assessment']['name'];
 
-// (Optional) Setup the paper size and orientation
-    $dompdf->setPaper('A4', 'landscape');
+    // Define a new file name (you can change this according to your needs)
+    $newFileName = 'renamed_file_' . time() . '.' . pathinfo($fileName, PATHINFO_EXTENSION);
 
-// Render the HTML as PDF
-    $dompdf->render();
+    // Full path to save the file
+    $destinationPath = $uploadDir . $newFileName;
 
-// Output the generated PDF to Browser
-    $dompdf->stream();
-
-} else{
-    echo 'Process Failed!';
+    // Move the file from the temporary directory to the final destination
+    if (move_uploaded_file($fileTmpPath, $destinationPath)) {
+        echo "File uploaded and renamed to: " . $newFileName;
+    } else {
+        echo "There was an error moving the uploaded file.";
+    }
 }
-
 die();
